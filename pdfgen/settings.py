b0f45a57e0963dda7241b0c83f1f5f2e4845b12a
MastewalB <se.mastewal.belete@gmail.com>
2024-02-04 01:19:27 +0300

    Add Database Configuration

--- pdfgen/settings.py
+++ pdfgen/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$x(hroj&)a-%zv77g1fmlkp593w9(6fg0gx59s50#d1#7ua)a5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://pdf-service-loev.onrender.com']
 
@@ -71,16 +74,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pdfgen.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
+DEBUG_DB_CONFIG = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+PROD_DB_CONFIG = {
+    'default': {
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USERNAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = DEBUG_DB_CONFIG if DEBUG else PROD_DB_CONFIG
+
 
 
 # Password validation
