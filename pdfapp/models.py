9fa68ee1e170e504f59f7dc55e638c21c383a507
MastewalB <se.mastewal.belete@gmail.com>
2024-03-10 21:07:11 +0300

    stripe backend integration

--- pdfapp/models.py
+++ pdfapp/models.py
@@ -6,3 +6,4 @@ class UserResponse(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     response = models.JSONField()
+    paid = models.BooleanField(default=False)
--- pdfapp/urls.py
+++ pdfapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pdfapp.views import index, UserResponseView, PDFView
+from pdfapp.views import index, UserResponseView, PDFView, PaymentView
 
 urlpatterns = [
     path('insert/', UserResponseView.as_view()),
+    path('payment/', PaymentView.as_view()),
     path('view/<str:email>', PDFView.as_view())
 ]
